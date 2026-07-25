69dfd668623773636ec6e38b46799a200bd5fe41
Francisco Cedermaz <francedermaz@gmail.com>
2026-07-25 00:18:43 -0300

    feat: add core SDK types and error class

--- tests/unit/smoke.spec.ts
+++
@@ -1,9 +0,0 @@
-describe('toolchain smoke test', () => {
-  it('should run TypeScript tests via ts-jest', () => {
-    // Given
-    const value: number = 1 + 1;
-
-    // Then
-    expect(value).toBe(2);
-  });
-});
